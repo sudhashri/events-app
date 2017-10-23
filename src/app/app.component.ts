3b5e10939206a801bb04d961937fa5d5863ad2bd
Sudhashri Hebbar <hebbars@vmware.com>
2017-10-23 10:24:00 +0530

    Added Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, OnInit } from '@angular/core';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(public _toastr: ToastsManager, _vcr: ViewContainerRef) {
+    this._toastr.setRootViewContainerRef(_vcr);
+  }
+
+  ngOnInit() {
+    this._toastr.success('App loaded!', 'Success!');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastModule } from 'ng2-toastr/ng2-toastr';
 
 import { AppComponent } from './app.component';
 import { EventListComponent } from './event-list/event-list.component';
 import { EventThumbnailComponent } from './event-thumbnail/event-thumbnail.component';
 import { NavComponent } from './nav/nav.component';
+import { EventListService } from './event-list/event-list.service';
 
 @NgModule({
   declarations: [
@@ -14,9 +17,11 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ToastModule.forRoot()
   ],
-  providers: [],
+  providers: [EventListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
